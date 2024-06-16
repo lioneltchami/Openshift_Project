6eddec49a49a3fd18ed01e33f39feb5e90e252e4
Lionel Tchami <65927599+apotitech@users.noreply.github.com>
2024-06-16 15:08:56 -0700

    UPDATING CODE

--- web_frontend/app.py
+++ web_frontend/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB2_URI')
+db = SQLAlchemy(app)
+
+# API server URL
+API_SERVER_URL = os.getenv('API_SERVER_URL')
 
 @app.route('/')
 def home():
