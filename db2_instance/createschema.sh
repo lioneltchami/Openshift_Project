b7eed3a37c15d34ce6570822a39eb612c432d97d
apotitech <lioneltchami@gmail.com>
2024-06-24 18:34:25 -0700

    db2 changes

--- db2_instance/createschema.sh
+++ db2_instance/createschema.sh
@@ -126,16 +126,41 @@
 
 ###################################
 
+# #!/bin/bash
+
+# # Wait for DB2 to start
+# until db2 connect to testdb; do
+#   echo "Waiting for DB2 to start..."
+#   sleep 5
+# done
+
+# # Create a sample table
+# db2 'CREATE TABLE IF NOT EXISTS sample_table (id INT PRIMARY KEY, name VARCHAR(100))'
+
+# # Insert sample data
+# db2 'INSERT INTO sample_table (id, name) VALUES (1, "Sample Data")'
+
+# # Disconnect from the database
+# db2 disconnect testdb
+
+####################################
+
 #!/bin/bash
 
-# Wait for DB2 to start
-until db2 connect to testdb; do
-  echo "Waiting for DB2 to start..."
-  sleep 5
-done
+# sleep 10
+sleep 10
+
+# Start the DB2 instance
+db2start
+
+# Create the database
+db2 create database testdb
+
+# Connect to the database
+db2 connect to testdb
 
 # Create a sample table
-db2 'CREATE TABLE IF NOT EXISTS sample_table (id INT PRIMARY KEY, name VARCHAR(100))'
+db2 'CREATE TABLE sample_table (id INT PRIMARY KEY, name VARCHAR(100))'
 
 # Insert sample data
 db2 'INSERT INTO sample_table (id, name) VALUES (1, "Sample Data")'
